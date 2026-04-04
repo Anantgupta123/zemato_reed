ebd05d00b113ade55bff0afc670f71ea0b0d5b28
Anant gupta <guptaanant382@gmail.com>
2026-04-04 19:18:40 +0530

    Completed project

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,17 @@
 const express  = require("express");
 const cookieparser = require("cookie-parser")
+const cors = require('cors');
 
 
 
 
 const app = express();
 
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieparser())
